b19469c85f62bf5c450451274f766d9ee21714a7
service@heisekeji.com <forkMm135642_>
2020-10-15 17:38:41 +0800

    修改存档bug 和 添加部分邀请好友的组件

--- assets/Script/UI/InviteFriend/InviteFriendItem.ts
+++ assets/Script/UI/InviteFriend/InviteFriendItem.ts
@@ -1,28 +1,23 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+import BaseNode from "../../Common/BaseNode";
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+class InviteFriendItem extends BaseNode {
 
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
+  
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    onLoad () {
+        super.onLoad();
+    }
 
     start () {
-
+        super.start();
+        
     }
 
     // update (dt) {}
 }
+
+export default InviteFriendItem
