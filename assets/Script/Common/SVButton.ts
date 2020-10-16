9c14f91db548b4794d5a21c4e4c7c6957d15dd48
uioracle5 <forkMm135642_>
2020-10-16 20:34:37 +0800

    添加SDK

---
+++ assets/Script/Common/SVButton.ts
@@ -0,0 +1,19 @@
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+class SVButton  extends cc.Component {
+
+    onLoad () {
+
+    }
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
+
+
+export default SVButton;
--- assets/Script/EventManager/EventConfig.ts
+++ assets/Script/EventManager/EventConfig.ts
@@ -1,6 +1,14 @@
 // 游戏内部的事件配置信息
 const EventConfig:{[key: string]:string} = {
-      "test":"test"
+      "test":"test",
+      /**@description 客户端告诉前端刷新数据 ：目前用于退出直客界面，刷新每日福利数据 */
+      "webReload":"webReload",
+      /**@description 安卓手机返回键响应 */
+      "webBackPage":"webBackPage",
+      /**@description 小电视数据响应 */
+      "webAlertMoney":"webAlertMoney",
+      /**@description 展示静态广告，红包上移响应 */
+      "webUpMoney":"webUpMoney",
 }
 
 export default EventConfig;
