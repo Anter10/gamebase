7d87c14d79a4abf549a731af5c9f3e9a917c4a74
uioracle5 <forkMm135642_>
2020-10-29 10:19:43 +0800

    修改测试脚本

--- tool/pack.js
+++ tool/pack.js
@@ -5,13 +5,18 @@ function execute(cmd) {
     exec(cmd, { stdio: 'inherit' });
 }
 
+const build_argv = {};
 // 构建参数
 function build_parameters() {
-
+    build_argv.branch = process.env.branch;
+    return build_argv;
 }
 
 
 function build_game() {
+    const build_argv = build_parameters();
+    console.log("构建参数 = ",build_argv);
+    
     // 编译项目
     execute(`/Applications/CocosCreator/Creator/2.4.3/CocosCreator.app/Contents/MacOS/CocosCreator --path ./ --build "platform=android;apiLevel=30;apilevel=30;debug=true;previewWidth=750;previewHeight=1134"`)
     console.log("build game succeed.");
