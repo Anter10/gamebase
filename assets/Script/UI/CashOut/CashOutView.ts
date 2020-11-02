344e9f7a1e2b63835bf0ca2b957bcda579bfe1bc
uioracle5 <forkMm135642_>
2020-11-02 10:42:28 +0800

    提现的功能

---
+++ assets/Script/UI/CashOut/CashOutView.ts
@@ -0,0 +1,48 @@
+import BaseUI from "../../Common/BaseUI";
+import { BaseUIInterface } from "../../Common/CommonInterface";
+import { CashOutViewInterface } from "./CashOutInterface";
+import {CashOutType} from "./CashOutEnum";
+import CashOutController from "./CashOutController";
+
+ 
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+/**@description 用户的提现界面 */
+class CashOutView extends BaseUI implements CashOutViewInterface, BaseUIInterface {
+    public cash_out_type: CashOutType = CashOutType.no_balance;
+    public controller: CashOutController = null;
+
+    @property(cc.Label)
+    moeny_label: cc.Label = null;
+
+    /**@description 刷新余额显示 */
+    flush_money(money: number){
+        this.moeny_label.string = `${money}`;
+    }
+
+    /**@description 提现调用 */
+    cash_out_callback(){
+        
+    }
+
+    onLoad () {
+        super.onLoad();
+        this.controller.init_view();
+    }
+
+    onAddFinished(){
+        super.onAddFinished();
+        this.controller.view = this;
+        this.controller.update_cash_out_view();
+    }
+    
+    start () {
+        super.start();
+    }
+
+    // update (dt) {}
+}
+
+
+export default  CashOutView;
---
+++ assets/Script/UI/CashOut/NoBalance/NoBanlanceCashOutView.ts
@@ -0,0 +1,56 @@
+import BaseUI from "../../../Common/BaseUI";
+import { UIParamInterface } from "../../../Common/CommonInterface";
+import Loader from "../../../Common/Loader";
+import UIConfig from "../../UIManager/UIConfig";
+import UIManager from "../../UIManager/UIManager";
+import { CashOutViewItemInterface } from "../CashOutInterface";
+import NoBalanceCashOutViewItem from "./NoBalanceCashOutViewItem";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+class NoBanlanceCashOutView extends BaseUI {
+
+    @property(cc.Node)
+    container: cc.Node = null;
+
+    @property(cc.Prefab)
+    no_banlance_view_item_prefab: cc.Prefab = null;
+
+    onLoad () {
+        super.onLoad();
+        this.add_nagivator([{action_title: "提现记录",action: () =>{
+            const ui_param_interface: UIParamInterface = {
+                ui_config_path: UIConfig.CashOutOrderView,
+                ui_config_name: "CashOutOrderView",
+                param:{}
+            }
+            UIManager.show_ui(ui_param_interface);
+        }}], 
+        {
+            title:"提现",
+            back_callback:()=>{
+                this.on_close_call("CashOutView");
+            }
+        })
+    }
+
+    update_view(with_draw_items: Array<CashOutViewItemInterface>){
+        this.container.removeAllChildren(true);
+        for(const item of with_draw_items){
+            const no_balance_node = cc.instantiate(this.no_banlance_view_item_prefab);
+            const no_balance_node_script: NoBalanceCashOutViewItem = no_balance_node.getComponent(NoBalanceCashOutViewItem);
+            no_balance_node_script.update_view(item);
+            no_balance_node.parent = this.container;
+        }
+    }
+
+    start () {
+    }
+
+    update (dt) {
+    }
+}
+
+
+export default NoBanlanceCashOutView;
