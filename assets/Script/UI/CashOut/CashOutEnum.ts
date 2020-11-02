37453eb5da4c9bbe83de61d096b20b71d7615a92
uioracle5 <forkMm135642_>
2020-11-02 10:42:28 +0800

    提现的功能

---
+++ assets/Script/UI/CashOut/CashOutEnum.ts
@@ -0,0 +1,11 @@
+
+/**@description 提现界面的类型 有余额的类型 和 非余额的类型 */
+enum CashOutType{
+    /**@description  1: 有余额类型的 */
+    balance = 1,
+    /**@description 2： 无余额类型的 */
+    no_balance = 2,
+}
+
+
+export {CashOutType};
---
+++ assets/Script/UI/CashOut/CashOutInterface.ts
@@ -0,0 +1,92 @@
+/**@description *新* 邀请数据 */
+interface InviteDataInterface{
+    limitMoney:number;
+    process: number;
+    needProcess: number;
+}
+
+/**@description 物品类信息 */
+interface GoodsDataInterface{
+    name: string;
+    content: string;
+    image: string;
+    money: string;
+}
+
+/**@description 用户的现金数据 */
+class UserMoneyModel{
+    readonly money: number;
+    readonly newUser: boolean;
+    readonly nickName: string;
+    readonly photoUrl:string;
+    readonly userId: string;
+}
+
+/**@description 提现界面的列表单个Item数据接口*/
+interface CashOutViewItemInterface{
+    /**@description 是否禁用  */
+    readonly disable?: boolean;
+    /**@description 提现类型 */
+    readonly goodsType?: number;
+    /**@description id  */
+    readonly id: number;
+    /**@description 金额 */
+    readonly money?:number;
+    /**@description 所需进度 */
+    readonly needProcess?: number;
+    /**@description 当前进度 */
+    readonly process?: number;
+    /**@description 类型 0: 普通, 1:新人, 2:每日, 3:打卡, 4:通关数, 5:商人订单初级, 6:商人订单中级, 7:商人订单高级 8:欺骗功能,不会完成 */
+    readonly type?: number;
+    /**@description 物品类信息 */
+    readonly goodsData?:GoodsDataInterface;
+    /**@description *新* 邀请数据 */
+    readonly inviteData?:InviteDataInterface;
+}
+
+/**@description 提现列表的数据接口 [GET: /api/withdraw]  */
+interface CashOutInterface{
+    items: Array<CashOutViewItemInterface>;
+}
+
+/**@description 提现界面的刷新接口 */
+interface CashOutViewInterface{
+    flush_money:Function;
+    cash_out_callback: Function;
+}
+
+/**@description 提现操作的数据接口[POST: /api/withdraw] */
+interface CashInterface{
+    /**@description 是否金额 */
+    readonly disable: boolean;
+    /**@description 新的余额 */
+    readonly newMoney?:  number;
+    /**@description 新的进度 */
+    readonly newProgress?: number;
+}
+
+/**@description 提现记录的数据接口[GET: /withdraw/order] */
+interface CashOutOrderInterface{
+    /**@description 消耗金额 */
+    readonly costMoney?: number;
+    /**@description 提现金额 */
+    readonly money?: number;
+    /**@description 提现状态 */
+    readonly status?: number;
+    /**@description 提现日期 */
+    readonly datetime?: string;
+}
+
+/**@description  提现记录 */
+interface OrderInterface{
+    items: Array<CashOutOrderInterface>
+}
+
+
+/**@description 提现成功的界面接口 */
+interface CashOutSuccessViewInterface{
+    
+}
+
+
+export {OrderInterface, CashOutInterface, CashOutOrderInterface, CashInterface, InviteDataInterface, GoodsDataInterface,UserMoneyModel, CashOutViewItemInterface,CashOutViewInterface, CashOutSuccessViewInterface};
