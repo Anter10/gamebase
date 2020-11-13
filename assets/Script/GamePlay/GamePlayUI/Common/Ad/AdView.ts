1d729b5075459898c2022a6ae59e1b07209c2824
qingsibaixue <duyu@coohua.com>
2020-11-13 20:05:05 +0800

    fix bug

--- assets/Script/GamePlay/GamePlayUI/Common/Ad/AdView.ts
+++ assets/Script/GamePlay/GamePlayUI/Common/Ad/AdView.ts
@@ -1,6 +1,9 @@
 import BaseUI from "../../../../Common/BaseUI";
 import { UIParamInterface } from "../../../../Common/CommonInterface";
 import TouchButton from "../../../../Common/TouchButton";
+import { Ad } from "../../../../Sdk/Ad";
+import { NativeSupportStatueCode } from "../../../../Sdk/SdkEnum";
+import { StaticImageAdInterface } from "../../../../Sdk/SdkInterface";
 
 const { ccclass, property } = cc._decorator;
 
@@ -23,6 +26,9 @@ export default class AdView extends BaseUI {
     @property(cc.Node)
     close_button: cc.Node = null;
 
+    @property(cc.Node)
+    bg: cc.Node = null;
+    
     ad_interface: AdInterface = null;
 
     onEnable() {
@@ -63,5 +69,43 @@ export default class AdView extends BaseUI {
 
     click_close_button() {
         this.on_close_call();
+    }
+
+
+    update_view_widget(code: NativeSupportStatueCode){
+        const widget = this.bg.getComponent(cc.Widget);
+        if(code == NativeSupportStatueCode.LOAD_FAIL){
+            widget.isAlignTop = false;
+            widget.isAlignVerticalCenter = true;
+            widget.updateAlignment();
+        }else{
+            widget.isAlignTop = true;
+            widget.top = 100;
+            widget.isAlignVerticalCenter = false;
+            widget.updateAlignment();
+        }
+    }
+
+    onAddFinished() {
+        const ad_data: StaticImageAdInterface = {
+            width:340,
+            height:250,
+            bottom: 0,
+            type:1,
+            success:(code: NativeSupportStatueCode) => {
+                console.log("静态图加载成功",code);
+                this.update_view_widget(code);
+            },
+            fail:(code: NativeSupportStatueCode) => {
+                console.log("静态图加载失败",code);
+                this.update_view_widget(code);
+            }
+        }
+        Ad.show_bottom_static_ad(ad_data);
+    }
+
+    onDisable() {
+        super.onDisable();
+        Ad.close_image_ad_view();
     }
 }
